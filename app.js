438db12e596d256ec2a81636c47a1f7f3d4e1ba3
chatBotfirstApp <m16652.hexaware@gmail.com>
2018-02-05 10:55:27 +0530

    test

--- app.js
+++ app.js
@@ -10,15 +10,16 @@ app.use(bodyParser.json());
    app.post('/',function(req,res){
       console.log('Received the request & it is:::'+JSON.stringify(req.body));
       if(req.body.result.action=='input.personaldetails')
-      {
-      var resagent='Your Ticket has been booked ';
+        {     var resagent='Your Ticket has been booked ';
      console.log('request are'+resagent);
       return res.json({
         speech:resagent,
         displayText: resagent,
         source:'Flight Booking'
       });
+    }
 });
+
 
 
 app.listen(portC, function(){
