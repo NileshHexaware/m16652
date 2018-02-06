1bb3b0604aea13f9d4d3560d3d90d01c1251c943
chatBotfirstApp <m16652.hexaware@gmail.com>
2018-02-06 21:19:19 +0530

    dsf

--- app.js
+++ app.js
@@ -6,20 +6,65 @@ var app = express();
 var portC = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
+var fbresponse="";
 
    app.post('/',function(req,res){
-      console.log('Received the request & it is:::'+JSON.stringify(req.body));
-      if(req.body.result.action=='input.personaldetails')
-        {     var resagent='Your Ticket has been booked ';
-     console.log('request are'+resagent);
-      return res.json({
-        speech:resagent,
-        displayText: resagent,
-        source:'Flight Booking'
-      });
-    }
-});
 
+    if (req.body.result.action==='Network')
+    {
+
+var fbresponse={
+
+
+    "speech": "",
+    "messages": [
+      {
+        "type": 1,
+        "platform": "facebook",
+        "title": "Servicenow",
+        "subtitle": "Servicenow",
+        "imageUrl": "https://botw-pd.s3.amazonaws.com/styles/logo-thumbnail/s3/122013/untitled-1_86.png?itok=jqHZFAoG",
+        "buttons": [
+          {
+            "text": "Network",
+            "postback": ""
+          },
+          {
+            "text": "Hardware",
+            "postback": ""
+          },
+          {
+            "text": "Software",
+            "postback": ""
+          }
+        ]
+      },
+      {
+        "type": 0,
+        "speech": ""
+      }
+    ]
+
+
+
+
+
+
+
+    //   console.log('Received the request & it is:::'+JSON.stringify(req.body));
+    //   if(req.body.result.action=='input.personaldetails')
+    //     {     var resagent='Your Ticket has been booked ';
+    //  console.log('request are'+resagent);
+    //   return res.json({
+    //     speech:resagent,
+    //     displayText: resagent,
+    //     source:'Flight Booking'
+    //   });
+    // }
+  }
+}
+});
+ 
 
 
 app.listen(portC, function(){
