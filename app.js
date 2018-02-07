5176c0197be25eda4aaf708452b174696eaeea86
chatBotfirstApp <m16652.hexaware@gmail.com>
2018-02-07 09:54:46 +0530

    d

--- app.js
+++ app.js
@@ -130,19 +130,19 @@ return res.json(fbresponse);
  
 //Rest Api Call started
 
-    incident.logIncident("incident 1111",'',function(err,resu){
-      var success=resu["result"]["number"];
-      });
-        console.log("Receives the request and number is"+JSON.stringify(req.body))
+    // incident.logIncident("incident 1111",'',function(err,resu){
+    //   var success=resu["result"]["number"];
+    //   });
+    //     console.log("Receives the request and number is"+JSON.stringify(req.body))
    
-       if(req.body.result.action=="Monitor"){
-        var resagent="Your incident has been created with incident number"+success;
-        }
-           return res.json({
-              speech:resagent,
-              displayText: resagent,
-              source:'Service Now'
-          });
+    //    if(req.body.result.action=="Monitor"){
+    //     var resagent="Your incident has been created with incident number"+success;
+    //     }
+    //        return res.json({
+    //           speech:resagent,
+    //           displayText: resagent,
+    //           source:'Service Now'
+    //       });
 
 
 app.listen(portC, function(){
