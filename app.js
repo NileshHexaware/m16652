cbd51298f08242a81807339b13edafedd9808143
chatBotfirstApp <m16652.hexaware@gmail.com>
2018-02-09 09:49:32 +0530

    hi

--- app.js
+++ app.js
@@ -147,32 +147,22 @@ return res.json(fbresponse);
       
 if(req.body.result.action=="getincident")
 {
-  incident.statusIncident(req.body.result.parameters.incidentno,function(err,resu){
+  incident.statusIncident(req.body.result.parameters.incidentno,function(err,resul){
   //var jsonparse= JSON.parse(resu);
-    var incdesc=resu["result"]["description"];
-    var incresolved_at=resu["result"]["resolved_at"];
-    var incbusiness_stc=resu["result"]["business_stc"];
-    var incsys_mod_count=resu["result"]["sys_mod_count"];
-
+    var incdesc=resul["result"]["description"];
+    var incresolved_at=resul["result"]["resolved_at"];
+    var incbusiness_stc=resul["result"]["business_stc"];
+    var incsys_mod_count=resul["result"]["sys_mod_count"];
     var incstatus="Your incident status is below \n 1.Incident Descrption = "+incdesc +"\n 2.Incident resolved on = "+incresolved_at+"\n 3.Incident business status = "+incbusiness_stc+"\n 4.Incident system mode count = "+incsys_mod_count;
-    
     return res.json( {
     speech:incstatus,
     displayText:incstatus,
-    source:'Service Now',
-    followupEvent:{
-      "name":"startmenmenu",
-      "data":{
-
-      }
-    }
- 
+    source:'Service Now'
     });
-  
   });
 }
 
-    });
+});
 
 app.listen(portC, function(){
     console.log('AGENT is running my app on  PORT: ' + portC);
