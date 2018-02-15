43e12c680acc8951c9cd0dbc104ddfbc3f91d4a5
chatBotfirstApp <m16652.hexaware@gmail.com>
2018-02-15 12:54:34 +0530

    hi

--- googleAssistConfig.js
+++ googleAssistConfig.js
@@ -1,17 +1,14 @@
-const ActionsSdkApp = require('actions-on-google').DialogflowApp;
-const app = new ActionsSdkApp({ request: request, response: response });
 
-// simple response
-   module.export={
-    'simpleresponse':function (app) {
-    app.ask(
-     app .buildRichResponse()
-      .addSimpleResponse('This is a simple response for suggestions')
-    )
-},
+var funResponse=function (req,res){
+    const ActionsSdkApp = require('actions-on-google').DialogflowApp;
+    const app = new ActionsSdkApp({ request: request, response: response });  
+    
+ var dispText = app.buildRichResponse()
+ .addSimpleResponse({speech: 'Please select option from ',
+   displayText: 'Please select option from '});
+ 
+   app.ask(dispText);
 
-'Simplesponse':function (app) {
-    app.ask('This is a simple response for suggestions')
-    }
 }
-   
+
+module.exports.simpleResponse=funResponse;
---
+++ test.js
@@ -0,0 +1,3 @@
+var ent=require('./googleAssistConfig.js');
+
+ent.abcd();
