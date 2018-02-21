cde1a0d748980bfb1a4f1aeaa1d5933e46f565db
chatBotfirstApp <m16652.hexaware@gmail.com>
2018-02-21 11:26:07 +0530

    slack code added

--- app.js
+++ app.js
@@ -173,6 +173,170 @@ if(req.body.result.action=="getincident")
 
 }
    }
+   else if(req.body.originalRequest.source=='slack'){
+    
+    if (req.body.result.parameters.Category=== 'Network')
+    {
+      var fbresponse={
+        "speech": "",
+        "messages": [
+          {
+            "type": 1,
+            "platform": "slack",
+            "title": "Servicenow",
+            "subtitle": "Servicenow",
+            "imageUrl": "https://botw-pd.s3.amazonaws.com/styles/logo-thumbnail/s3/122013/untitled-1_86.png?itok=jqHZFAoG",
+            "buttons": [
+              {
+                "text": "DHCP",
+                "postback": "DHCP"
+              },
+              {
+                "text": "DNS",
+                "postback": "DNS"
+              },
+              {
+                "text": "IP",
+                "postback": "IP"
+              }
+            ]
+          },
+          {
+            "type": 0,
+            "speech": ""
+          }
+        ]
+    
+      };
+      return res.json(fbresponse);
+    }
+    
+      if (req.body.result.parameters.Category=== 'Hardware')
+      {
+      var fbresponse={
+      "speech": "",
+      "messages": [
+        {
+          "type": 1,
+          "platform": "slack",
+          "title": "Servicenow",
+          "subtitle": "Servicenow",
+          "imageUrl": "https://botw-pd.s3.amazonaws.com/styles/logo-thumbnail/s3/122013/untitled-1_86.png?itok=jqHZFAoG",
+          "buttons": [
+            {
+              "text": "Monitor",
+              "postback": "Monitor"
+            },
+            {
+              "text": "Keyboard",
+              "postback": "Keyboard"
+            },
+            {
+              "text": "Mouse",
+              "postback": "Mouse"
+            }
+          ]
+        },
+        {
+          "type": 0,
+          "speech": ""
+        }
+      ]
+    };
+    return res.json(fbresponse);
+      }
+    
+      if (req.body.result.parameters.Category=== 'Software')
+        {
+        var fbresponse={
+        "speech": "",
+        "messages": [
+          {
+            "type": 1,
+            "platform": "slack",
+            "title": "Servicenow",
+            "subtitle": "Servicenow",
+            "imageUrl": "https://botw-pd.s3.amazonaws.com/styles/logo-thumbnail/s3/122013/untitled-1_86.png?itok=jqHZFAoG",
+            "buttons": [
+              {
+                "text": "Email",
+                "postback": "Email"
+              },
+              {
+                "text": "OS",
+                "postback": "OS"
+              },
+              {
+                "text": "Mac",
+                "postback": "Mac"
+              }
+            ]
+          },
+          {
+            "type": 0,
+            "speech": ""
+          }
+        ]
+      };
+      return res.json(fbresponse);
+    }
+    
+    //Rest Api Call started
+    
+      if(req.body.result.action=="CreateIncident.CreateIncident-custom"){
+    
+        var cat=req.body.result.contexts[0].parameters.Category;
+         incident.logIncident(req.body.result.parameters.desc,req.body.result.parameters.severity,cat,req.body.result.parameters.subcategory,function(err,resu){
+          var success=resu["result"]["number"];
+          var resagent="Your incident has been created with incident number:"+success +".\nNote it down for further enquiry.";
+        return res.json({
+          followupEvent :{
+            "name":"mainmenuevent",
+            "data":{
+              "incnumber":success
+            }
+          }
+        });
+      });
+    }
+          
+    if(req.body.result.action=="getincident")
+    {
+      incident.statusIncident(req.body.result.parameters.incidentno,function(err,resul){
+       var jsonparse= JSON.parse(resul);
+       if(jsonparse.hasOwnProperty('result'))
+        {
+        console.log(jsonparse.result[0].description);
+        return res.json( {
+        followupEvent :{
+          "name":"mainmenueventgetinc",
+          "data":{
+            "incstatus":jsonparse.result[0].description,
+            "incnumber":jsonparse.result[0].number,
+            "resolved_at":jsonparse.result[0].resolved_at
+          }
+        }
+       });
+      }
+      else
+      {
+        return res.json({
+          followupEvent :{
+            "name":"IncFailevent",
+            "data":{
+             
+            }
+          }
+    
+        });
+        
+      }
+      });
+    
+    }
+    
+  }
+
    else
    {
     if (req.body.result.parameters.Category=== 'Network')
