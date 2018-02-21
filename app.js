13f102a01d62ebc36139ae87d262bc4101e31da4
chatBotfirstApp <m16652.hexaware@gmail.com>
2018-02-21 15:19:18 +0530

    commit

--- app.js
+++ app.js
@@ -123,6 +123,7 @@ return res.json(fbresponse);
 //Rest Api Call started
 
   if(req.body.result.action=="CreateIncident.CreateIncident-custom"){
+    var cat=req.body.result.parameters.Category;
      incident.logIncident(req.body.result.parameters.desc,req.body.result.parameters.severity,cat,req.body.result.parameters.subcategory,function(err,resu){
       var success=resu["result"]["number"];
       var resagent="Your incident has been created with incident number:"+success +".\nNote it down for further enquiry.";
