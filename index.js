e2d7aa82e4494b1aba0ed04ca7d70b0f4d251339
chatBotfirstApp <m16652.hexaware@gmail.com>
2018-02-22 11:00:23 +0530

    hi

--- index.js
+++ index.js
@@ -18,31 +18,72 @@ var fbresponse={
   "speech": "",
   "messages": [
     {
-      "type": 1,
-      "platform": "facebook",
-      "title": "Servicenow",
-      "subtitle": "Servicenow",
-      "imageUrl": "https://botw-pd.s3.amazonaws.com/styles/logo-thumbnail/s3/122013/untitled-1_86.png?itok=jqHZFAoG",
-      "buttons": [
-        {
-          "text": "DHCP",
-          "postback": "DHCP"
-        },
-        {
-          "text": "DNS",
-          "postback": "DNS"
-        },
-        {
-          "text": "IP Address",
-          "postback": "IP Address"
+      "attachment": {
+        "type": "template",
+        "payload": {
+          "template_type": "list",
+          "top_element_style": "compact",
+          "elements": [
+            {
+              "title": "Classic T-Shirt Collection",
+              "subtitle": "See all our colors",
+              "image_url": "https://peterssendreceiveapp.ngrok.io/img/collection.png",          
+              "buttons": [
+                {
+                  "title": "View",
+                  "type": "web_url",
+                  "url": "https://peterssendreceiveapp.ngrok.io/collection",
+                  "messenger_extensions": true,
+                  "webview_height_ratio": "tall",
+                  "fallback_url": "https://peterssendreceiveapp.ngrok.io/"            
+                }
+              ]
+            },
+            {
+              "title": "Classic White T-Shirt",
+              "subtitle": "See all our colors",
+              "default_action": {
+                "type": "web_url",
+                "url": "https://peterssendreceiveapp.ngrok.io/view?item=100",
+                "messenger_extensions": false,
+                "webview_height_ratio": "tall"
+              }
+            },
+            {
+              "title": "Classic Blue T-Shirt",
+              "image_url": "https://peterssendreceiveapp.ngrok.io/img/blue-t-shirt.png",
+              "subtitle": "100% Cotton, 200% Comfortable",
+              "default_action": {
+                "type": "web_url",
+                "url": "https://peterssendreceiveapp.ngrok.io/view?item=101",
+                "messenger_extensions": true,
+                "webview_height_ratio": "tall",
+                "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+              },
+              "buttons": [
+                {
+                  "title": "Shop Now",
+                  "type": "web_url",
+                  "url": "https://peterssendreceiveapp.ngrok.io/shop?item=101",
+                  "messenger_extensions": true,
+                  "webview_height_ratio": "tall",
+                  "fallback_url": "https://peterssendreceiveapp.ngrok.io/"            
+                }
+              ]        
+            }
+          ],
+           "buttons": [
+            {
+              "title": "View More",
+              "type": "postback",
+              "payload": "payload"            
+            }
+          ]  
         }
-      ]
-    },
-    {
-      "type": 0,
-      "speech": ""
+      }
     }
-  ]
+  
+  ]     
 
 };
 
