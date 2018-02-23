c4229dd7589b4c02ffbb4e443492091c97f09209
chatBotfirstApp <m16652.hexaware@gmail.com>
2018-02-23 14:46:47 +0530

    hi

--- app2.js
+++ app2.js
@@ -10,34 +10,84 @@ var incident=require('./DAL');
 
 app.post('/',function(req,res){
   var fbresponse={
-    "speech": "",
+    "speech": "Incident No",
     "messages": [
-      {
-        "type": 1,
-        "platform": "facebook",
-        "title": "Servicenow",
-        "subtitle": "Servicenow",
-        "imageUrl": "https://botw-pd.s3.amazonaws.com/styles/logo-thumbnail/s3/122013/untitled-1_86.png?itok=jqHZFAoG",
-        "buttons": [
           {
-            "text": "DHCP",
-            "postback": "DHCP"
+            "type": 0,
+             "platform": "facebook",
+             "speech": "Please find status of  below."
           },
           {
-            "text": "DNS",
-            "postback": "DNS"
-          },
-          {
-            "text": "IP Address",
-            "postback": "IP Address"
-          }
+            "type": 4,
+            "platform": "facebook",
+             "payload": {
+             "facebook": {
+             "attachment": {
+              "type": "template",
+              "payload": {
+               "template_type": "list",
+               "top_element_style": "compact",
+                "elements": [
+                      {
+                         "title": "Status",
+                          "subtitle": "CLOSED"
+                      },
+                     {
+                        "title": "Priority",
+                         "subtitle": "CLOSED"
+                      },
+                      {
+                        "title": "Created On",
+                        "subtitle": "creatd";
+                      }
+                            ]
+                          }
+                            }
+                         }
+                            }
+              },
+              {
+                  "type": 2,
+                  "platform": "facebook",
+                   "title": "Would you like to do more ?",
+                   "replies": [
+                    "Yes",
+                     "No"
+                     ]
+              }
         ]
-      },
-      {
-        "type": 0,
-        "speech": ""
-      }
-    ]
+    
+
+
+    /////////////////////////////////////card///////////////////////////////////
+    // "speech": "",
+    // "messages": [
+    //   {
+    //     "type": 1,
+    //     "platform": "facebook",
+    //     "title": "Servicenow",
+    //     "subtitle": "Servicenow",
+    //     "imageUrl": "https://botw-pd.s3.amazonaws.com/styles/logo-thumbnail/s3/122013/untitled-1_86.png?itok=jqHZFAoG",
+    //     "buttons": [
+    //       {
+    //         "text": "DHCP",
+    //         "postback": "DHCP"
+    //       },
+    //       {
+    //         "text": "DNS",
+    //         "postback": "DNS"
+    //       },
+    //       {
+    //         "text": "IP Address",
+    //         "postback": "IP Address"
+    //       }
+    //     ]
+    //   },
+    //   {
+    //     "type": 0,
+    //     "speech": ""
+    //   }
+    // ]
 
   };
   return res.json(fbresponse);
