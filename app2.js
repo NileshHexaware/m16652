5d189d4eb19fbb4b1148f381c646818fb4abb378
chatBotfirstApp <m16652.hexaware@gmail.com>
2018-02-23 16:14:17 +0530

    hi

--- app2.js
+++ app2.js
@@ -1,17 +1,19 @@
 var request = require('http');
-var express=require('express');
+var express = require('express');
 var bodyParser = require('body-parser');
 var isAlphanumeric = require('is-alphanumeric');
 var app = express();
 var portC = process.env.PORT || 3000;
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-var incident=require('./DAL');
+var incident = require('./DAL');
 
-app.post('/',function(req,res){
+app.post('/', function (req, res) {
 
   console.log("app started nilesh");
-  var fbresponse={
+  var fbresponse = {
+
+
     "messages": [
       {
         "type": 4,
@@ -21,51 +23,58 @@ app.post('/',function(req,res){
             "attachment": {
               "type": "template",
               "payload": {
-                "template_type": "list",
-                "top_element_style": "compact",
+                "template_type": "generic",
                 "elements": [
                   {
-                    "title": "Classic T-Shirt Collection",
-                    "subtitle": "See all our colors",
-                    "image_url": "https://peterssendreceiveapp.ngrok.io/img/collection.png",
+                    "title": "Welcome!",
+                    "image_url": "https://auto.ndtvimg.com/bike-images/colors/suzuki/intruder/suzuki-intruder-glass-sparkle-black.png?v=14",
+                    "subtitle": "We have the right hat for everyone.",
                     "buttons": [
                       {
-                        "title": "View",
                         "type": "web_url",
-                        "url": "https://peterssendreceiveapp.ngrok.io/collection"
+                        "url": "https://petersfancybrownhats.com",
+                        "title": "View Website"
+                      },
+                      {
+                        "type": "postback",
+                        "title": "Start Chatting",
+                        "payload": "DEVELOPER_DEFINED_PAYLOAD"
                       }
                     ]
                   },
                   {
-                    "title": "Classic White T-Shirt",
-                    "subtitle": "See all our colors",
-                    "image_url": "https://peterssendreceiveapp.ngrok.io/img/collection.png",
+                    "title": "Welcome!",
+                    "image_url": "https://petersfancybrownhats.com/company_image.png",
+                    "subtitle": "We have the right hat for everyone.",
                     "buttons": [
                       {
-                        "title": "View",
                         "type": "web_url",
-                        "url": "https://peterssendreceiveapp.ngrok.io/collection"
+                        "url": "https://petersfancybrownhats.com",
+                        "title": "View Website"
+                      },
+                      {
+                        "type": "postback",
+                        "title": "Start Chatting",
+                        "payload": "DEVELOPER_DEFINED_PAYLOAD"
                       }
                     ]
                   },
                   {
-                    "title": "Classic Blue T-Shirt",
-                    "image_url": "https://peterssendreceiveapp.ngrok.io/img/blue-t-shirt.png",
-                    "subtitle": "100% Cotton, 200% Comfortable",
+                    "title": "Welcome!",
+                    "image_url": "https://petersfancybrownhats.com/company_image.png",
+                    "subtitle": "We have the right hat for everyone.",
                     "buttons": [
                       {
-                        "title": "Shop Now",
                         "type": "web_url",
-                        "url": "https://peterssendreceiveapp.ngrok.io/shop?item=101"
+                        "url": "https://petersfancybrownhats.com",
+                        "title": "View Website"
+                      },
+                      {
+                        "type": "postback",
+                        "title": "Start Chatting",
+                        "payload": "DEVELOPER_DEFINED_PAYLOAD"
                       }
                     ]
-                  }
-                ],
-                "buttons": [
-                  {
-                    "title": "View More",
-                    "type": "postback",
-                    "payload": "payload"
                   }
                 ]
               }
@@ -73,9 +82,73 @@ app.post('/',function(req,res){
           }
         }
       }
-    ]
-     
-    
+
+    ///////////////////////////////list////////////////////////////////
+
+    // "messages": [
+    //   {
+    //     "type": 4,
+    //     "platform": "facebook",
+    //     "payload": {
+    //       "facebook": {
+    //         "attachment": {
+    //           "type": "template",
+    //           "payload": {
+    //             "template_type": "list",
+    //             "top_element_style": "compact",
+    //             "elements": [
+    //               {
+    //                 "title": "Classic T-Shirt Collection",
+    //                 "subtitle": "See all our colors",
+    //                 "image_url": "https://peterssendreceiveapp.ngrok.io/img/collection.png",
+    //                 "buttons": [
+    //                   {
+    //                     "title": "View",
+    //                     "type": "web_url",
+    //                     "url": "https://peterssendreceiveapp.ngrok.io/collection"
+    //                   }
+    //                 ]
+    //               },
+    //               {
+    //                 "title": "Classic White T-Shirt",
+    //                 "subtitle": "See all our colors",
+    //                 "image_url": "https://peterssendreceiveapp.ngrok.io/img/collection.png",
+    //                 "buttons": [
+    //                   {
+    //                     "title": "View",
+    //                     "type": "web_url",
+    //                     "url": "https://peterssendreceiveapp.ngrok.io/collection"
+    //                   }
+    //                 ]
+    //               },
+    //               {
+    //                 "title": "Classic Blue T-Shirt",
+    //                 "image_url": "https://peterssendreceiveapp.ngrok.io/img/blue-t-shirt.png",
+    //                 "subtitle": "100% Cotton, 200% Comfortable",
+    //                 "buttons": [
+    //                   {
+    //                     "title": "Shop Now",
+    //                     "type": "web_url",
+    //                     "url": "https://peterssendreceiveapp.ngrok.io/shop?item=101"
+    //                   }
+    //                 ]
+    //               }
+    //             ],
+    //             "buttons": [
+    //               {
+    //                 "title": "View More",
+    //                 "type": "postback",
+    //                 "payload": "payload"
+    //               }
+    //             ]
+    //           }
+    //         }
+    //       }
+    //     }
+    //   }
+    // ]
+
+
 
 
     /////////////////////////////////////card///////////////////////////////////
@@ -113,8 +186,7 @@ app.post('/',function(req,res){
   return res.json(fbresponse);
 });
 
-app.listen(portC,function(req,res){
+app.listen(portC, function (req, res) {
   console.log('AGENT is running my app on  PORT: ' + portC);
 });
 
- 
