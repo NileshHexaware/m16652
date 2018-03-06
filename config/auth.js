8617a8726f968931a94aa1a29b1e6e08796c8ef6
chatBotfirstApp <m16652.hexaware@gmail.com>
2018-03-06 14:58:12 +0530

    hi

--- config/auth.js
+++ config/auth.js
@@ -3,7 +3,7 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      :'799109956956017', 
         'clientSecret'  : '730e8800f431b454eb89f8e339025058',
-        'callbackURL'   : 'https://flighthexaware.herokuapp.com/',
+        'callbackURL'   : 'https://flighthexaware.herokuapp.com/callback',
         'profileURL'    : 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email',
         'profileFields' : ['id', 'email', 'name'] // For requesting permissions from Facebook API
     },
