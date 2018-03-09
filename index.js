cf82c212bce52618166998ead0111a1edab38234
chatBotfirstApp <m16652.hexaware@gmail.com>
2018-03-09 16:20:32 +0530

    hi

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ app.post('/', function (req, res) {
     }
 
     if (req.body.result.parameters.Category === 'Network') {
-      return res.json(fbmodularity.subcategoryNetwork(req, resp));
+      return res.json(fbmodularity.subcategoryNetwork(req, res));
       console.log(resp);
       //bot=req.body.result.fulfillment.messages[3].title +'<br>'+'select below category -'+req.body.result.fulfillment.messages[3].buttons[0].text+'</br>'+'<br>'+req.body.result.fulfillment.messages[3].buttons[1].text+'</br>'+'<br>'+req.body.result.fulfillment.messages[3].buttons[2].text;
       incident.Chatlogs(req.body.result.resolvedQuery,bot,req.body.sessionId);
