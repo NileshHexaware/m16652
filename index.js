018f61461024ed74edd6e822ac4e5c5df26cb756
chatBotfirstApp <m16652.hexaware@gmail.com>
2018-03-12 10:25:20 +0530

    hi

--- index.js
+++ index.js
@@ -65,10 +65,8 @@ app.post('/', function (req, res) {
     }
 
     if (req.body.result.parameters.Category === 'Network') {
+      console.log("Nilesh Network");
       return res.json(fbmodularity.subcategoryNetwork(req, res));
-      // console.log(res);
-      //bot=req.body.result.fulfillment.messages[3].title +'<br>'+'select below category -'+req.body.result.fulfillment.messages[3].buttons[0].text+'</br>'+'<br>'+req.body.result.fulfillment.messages[3].buttons[1].text+'</br>'+'<br>'+req.body.result.fulfillment.messages[3].buttons[2].text;
-      //incident.Chatlogs(req.body.result.resolvedQuery,bot,req.body.sessionId);
     }
 
     if (req.body.result.parameters.Category === 'Hardware') {
